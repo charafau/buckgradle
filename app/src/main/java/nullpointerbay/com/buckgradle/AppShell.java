1390a63fd7a9ab24a0bc30403511c45f3cdef478
charafau <gorudonu@gmail.com>
2015-10-17 00:14:27 +0900

    Added first support for exopackage

---
+++ app/src/main/java/nullpointerbay/com/buckgradle/AppShell.java
@@ -0,0 +1,10 @@
+package nullpointerbay.com.buckgradle;
+
+import com.facebook.buck.android.support.exopackage.ExopackageApplication;
+
+public class AppShell extends ExopackageApplication {
+
+    public AppShell() {
+        super(1);
+    }
+}
